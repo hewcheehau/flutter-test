9b341f1ae99208918273b009f58e6a4edd48c961
hewcheehau <cheehau67@hotmail.com>
2025-05-15 23:15:03 +0800

    feat: add product details

--- lib/core/app_router.dart
+++ lib/core/app_router.dart
@@ -1,7 +1,25 @@
 import 'package:go_router/go_router.dart';
+import 'package:myeg_flutter_test/core/model/product.dart';
 import 'package:myeg_flutter_test/features/cart/view/cart_page.dart';
-import 'package:myeg_flutter_test/features/product/view/product_list_page.dart';
+import 'package:myeg_flutter_test/features/product/product.dart';
 
-final router = GoRouter(initialLocation: '/', routes: [GoRoute(name: 'products', path: '/', builder: (context, state) => ProductListPage()),
-GoRoute(name: 'cart', path: '/cart', builder: (context, state) => CartPage())
-]);
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      name: 'products',
+      path: '/',
+      builder: (context, state) => ProductListPage(),
+      routes: [
+        GoRoute(
+          path: 'details',
+          builder: (context, state) {
+            Product product = state.extra as Product;
+            return ProductDetailsPage(product: product);
+          },
+        ),
+      ],
+    ),
+    GoRoute(name: 'cart', path: '/cart', builder: (context, state) => CartPage()),
+  ],
+);
